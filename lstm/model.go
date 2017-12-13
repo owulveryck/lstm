d3ce32f9dd2f0f00b839ddb896bc627d93f0ff18
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2017-12-13 15:21:24 +0100

    WIP

--- lstm/model.go
+++ lstm/model.go
@@ -2,15 +2,25 @@ package lstm
 
 import G "gorgonia.org/gorgonia"
 
-type lstm struct{}
+type lstm struct {
+	wf *G.Node
+	bf *G.Node
+	wi *G.Node
+	bi *G.Node
+	wo *G.Node
+	bo *G.Node
+	wc *G.Node
+	bc *G.Node
+	g  G.ExprGraph
+}
 
-func (m *lstm) step(c, h, x *G.Node) (*G.Node, *G.Node) {
+func (m *lstm) step(cₜₜ, hₜₜ, xₜ *G.Node) (cₜ, hₜ *G.Node) {
 	// fₜ= σ(Wf·[hₜ₋₁,xₜ] + bf)
 	// iₜ= σ(Wi·[hₜ₋₁,xₜ] + bi)
 	// Ĉₜ= tanh(Wc·[hₜ₋₁,xₜ] + bc)
 	// Cₜ= fₜ* Cₜ₋₁+ iₜ* Ĉₜ
 	// oₜ= σ(Wo·[hₜ₋₁,xₜ] + bo)
-	// hₜ= oₜ*tanh(Cₜ)
+	// hₜ= oₜ* tanh(Cₜ)
 
 	return nil, nil
 }
