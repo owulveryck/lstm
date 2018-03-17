28927fccf60fd8cbecdff9d4b580047c56a6c0ec
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-03-17 14:32:13 +0100

    An attempt to change the interface

--- lstm/lstm.go
+++ lstm/lstm.go
@@ -1,62 +1,31 @@
 package lstm
 
 import (
-	"bufio"
 	"io"
 
 	G "gorgonia.org/gorgonia"
+	"gorgonia.org/tensor"
 )
 
-// IO is a structure that handles a batch training set
-type IO struct {
-	RS io.ReadSeeker
-	WS io.ReadWriteSeeker
-	*Reader
-}
-
-type Reader struct {
-	r   io.Reader
-	buf *bufio.Reader
-}
-
-func (i *IO) WriteVector(*G.Node) error {
-	return nil
-}
-
-func NewReader(r io.Reader) *Reader {
-	return &Reader{
-		r:   r,
-		buf: bufio.NewReader(r),
-	}
-}
-func (r *Reader) ReadVector() ([]float32, error) {
-	char, _, err := r.buf.ReadRune()
-	if err != nil {
-		return nil, err
-	}
-	return oneOfK(char)
-}
-
-func oneOfK(r rune) ([]float32, error) {
-	return nil, nil
-}
-func (i *IO) getLastOutputVector() (*G.Node, error) {
-	return nil, nil
+// dataSetIO is an interface that can Read and returns a oneOfK encoded vector
+type dataSetIO interface {
+	ReadInputVec() ([]float32, error)
+	WriteVec([]float32) error
 }
 
 // Forward pass as described here https://en.wikipedia.org/wiki/Long_short-term_memory#LSTM_with_a_forget_gate
-func (m *Model) fwd(tset IO, prevHidden, prevCell *G.Node, currentGeneratedIndex, maxGeneratedIndex int) error {
-	inputVector, err := tset.ReadVector()
+// It returns the last hidden node and the last cell node
+func (m *Model) fwd(dataSet dataSetIO, prevHidden, prevCell *G.Node) (*G.Node, *G.Node, error) {
+	// Read the current input vector
+	inputVectorValue, err := dataSet.ReadInputVec()
+	inputTensor := tensor.New(tensor.Of(tensor.Float32), tensor.WithBacking(inputVectorValue))
+	inputVector := G.NewVector(m.g, tensor.Float32, G.WithName("xₜ"), G.WithShape(m.inputSize), G.WithValue(inputTensor))
+
 	switch {
 	case err != nil && err != io.EOF:
-		return err
-	case err == io.EOF && currentGeneratedIndex == maxGeneratedIndex:
-		return nil
-	case err == io.EOF && currentGeneratedIndex < maxGeneratedIndex:
-		inputVector, err = tset.getLastOutputVector()
-		if err != nil {
-			return err
-		}
+		return prevHidden, prevCell, err
+	case err == io.EOF:
+		return prevHidden, prevCell, nil
 	}
 	// Helper function for clarity
 	set := func(ident, equation string) *G.Node {
@@ -76,6 +45,9 @@ func (m *Model) fwd(tset IO, prevHidden, prevCell *G.Node, currentGeneratedIndex
 	ct := set(`cₜ`, `fₜ*cₜ₋₁+iₜ*ĉₜ`)
 	set(`hc`, `tanh(cₜ)`)
 	ht, _ := m.parser.Parse(`oₜ*hc`)
-	tset.writeCurrentInputVector(ht)
-	return m.fwd(tset, ht, ct, currentGeneratedIndex+1, maxGeneratedIndex)
+	// Apply the softmax function to the output vector
+	prob := G.Must(G.SoftMax(ht))
+
+	dataSet.WriteVec(prob.Value().Data().([]float32))
+	return m.fwd(dataSet, prob, ct)
 }
