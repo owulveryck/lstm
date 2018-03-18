71f146fc5daa31754ae58e9aa906a1cbeddd87fe
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-03-18 22:11:05 +0100

    Code cleaning

--- parser/expr.go
+++ parser/expr.go
@@ -199,26 +199,6 @@ func (x *exprLex) Error(s string) {
 	}
 }
 
-/*
-func main() {
-	in := bufio.NewReader(os.Stdin)
-	for {
-		if _, err := os.Stdout.WriteString("> "); err != nil {
-			log.Fatalf("WriteString: %s", err)
-		}
-		line, err := in.ReadBytes('\n')
-		if err == io.EOF {
-			return
-		}
-		if err != nil {
-			log.Fatalf("ReadBytes: %s", err)
-		}
-
-		exprParse(&exprLex{line: line})
-	}
-}
-*/
-
 //line yacctab:1
 var gorgoniaExca = [...]int{
 	-1, 1,
--- parser/src/expr.y
+++ parser/src/expr.y
@@ -245,22 +245,3 @@ func (x *exprLex) Error(s string) {
 	}
 }
 
-/*
-func main() {
-	in := bufio.NewReader(os.Stdin)
-	for {
-		if _, err := os.Stdout.WriteString("> "); err != nil {
-			log.Fatalf("WriteString: %s", err)
-		}
-		line, err := in.ReadBytes('\n')
-		if err == io.EOF {
-			return
-		}
-		if err != nil {
-			log.Fatalf("ReadBytes: %s", err)
-		}
-
-		exprParse(&exprLex{line: line})
-	}
-}
-*/
