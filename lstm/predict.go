81070ea9741c003121a4ac2ef384cf14ef725c44
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-03-22 22:13:21 +0100

    Predict to be tested

--- lstm/predict.go
+++ lstm/predict.go
@@ -1,8 +1,25 @@
 package lstm
 
-import "context"
+import (
+	"context"
+
+	"github.com/owulveryck/charRNN/datasetter"
+	G "gorgonia.org/gorgonia"
+)
 
 // Predict ...
-func (m *Model) Predict(ctx context.Context) error {
+func (m *Model) Predict(ctx context.Context, dataSet datasetter.ReadWriter) error {
+	hidden, cell, err := m.forwardStep(dataSet, m.prevHidden, m.prevCell, 0)
+	if err != nil {
+		return err
+	}
+	g := m.g.SubgraphRoots(dataSet.GetComputedVectors()...)
+	machine := G.NewLispMachine(g)
+	if err := machine.RunAll(); err != nil {
+		return err
+	}
+
+	m.prevHidden = hidden
+	m.prevCell = cell
 	return nil
 }
