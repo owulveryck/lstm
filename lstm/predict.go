4fe84dc988d1a9ed95f1e9bc59ca50f4e38baf8d
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-03-23 17:09:42 +0100

    Predict should start with an empty memory and avoid updating it

--- lstm/predict.go
+++ lstm/predict.go
@@ -5,21 +5,23 @@ import (
 
 	"github.com/owulveryck/charRNN/datasetter"
 	G "gorgonia.org/gorgonia"
+	"gorgonia.org/tensor"
 )
 
 // Predict ...
 func (m *Model) Predict(ctx context.Context, dataSet datasetter.ReadWriter) error {
-	hidden, cell, err := m.forwardStep(dataSet, m.prevHidden, m.prevCell, 0)
+	// We need an empty memory to start...
+	hiddenT := tensor.New(tensor.Of(tensor.Float32), tensor.WithShape(m.hiddenSize))
+	cellT := tensor.New(tensor.Of(tensor.Float32), tensor.WithShape(m.hiddenSize))
+	prevHidden := G.NewVector(m.g, tensor.Float32, G.WithName("hₜ₋₁"), G.WithShape(m.hiddenSize), G.WithValue(hiddenT))
+	prevCell := G.NewVector(m.g, tensor.Float32, G.WithName("Cₜ₋₁"), G.WithShape(m.hiddenSize), G.WithValue(cellT))
+	_, _, err := m.forwardStep(dataSet, prevHidden, prevCell, 0)
 	if err != nil {
 		return err
 	}
 	g := m.g.SubgraphRoots(dataSet.GetComputedVectors()...)
 	machine := G.NewLispMachine(g, G.ExecuteFwdOnly())
-	if err := machine.RunAll(); err != nil {
-		return err
-	}
+	err = machine.RunAll()
+	return err
 
-	m.prevHidden = hidden
-	m.prevCell = cell
-	return nil
 }
