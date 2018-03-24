630a9c17104df26a5eafb6ac7f94d886869aecec
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-03-24 18:41:30 +0100

    Better design in progress

--- datasetter/char/trainingset.go
+++ datasetter/char/trainingset.go
@@ -11,74 +11,77 @@ import (
 
 // TrainingSet ...
 type TrainingSet struct {
-	rs             io.ReaderAt
-	currentSection *io.SectionReader
-	runeToIdx      func(r rune) (int, error)
-	vocabSize      int
-	idxToRune      func(idx int) (rune, error)
-	outputStream   io.ReadWriter
-	offset         int
-	output         G.Nodes
-	epoch          int
-	maxEpoch       int
+	rs        io.Reader
+	runeToIdx func(r rune) (int, error)
+	vocabSize int
+	step      int
 }
 
-// NewTrainingSet from a ReaderAt
-func NewTrainingSet(rs io.ReaderAt) *TrainingSet {
+// Section ...
+type Section struct {
+	sentence  []int
+	output    G.Nodes
+	vocabSize int
+	offset    int
+}
+
+// NewTrainingSet from a ReadSeeker
+func NewTrainingSet(rs io.Reader, runeToIdx func(r rune) (int, error), vocabSize, step int) *TrainingSet {
 	return &TrainingSet{
-		rs: rs,
+		rs:        rs,
+		vocabSize: vocabSize,
+		step:      step,
+		runeToIdx: runeToIdx,
 	}
 }
 
 // ReadInputVector returns the input vector until it reach the penultimate rune
 // the ultimate rune is not used as input within the current section as an input
-func (t *TrainingSet) ReadInputVector(g *G.ExprGraph) (*G.Node, error) {
-	b := bufio.NewReader(t.currentSection)
-	r, size, err := b.ReadRune()
-	if err != nil {
-		return nil, err
+func (s *Section) ReadInputVector(g *G.ExprGraph) (*G.Node, error) {
+	if s.offset == len(s.sentence) {
+		return nil, io.EOF
 	}
-	t.offset += size
-	// Check if the last rune is returning an error
-	_, _, err = b.ReadRune()
-	if err == io.EOF {
-		return nil, err
-	}
-	// put the rune back in the stream
-	err = b.UnreadRune()
-	if err != nil {
-		return nil, err
-	}
-	idx, err := t.runeToIdx(r)
-	if err != nil {
-		return nil, err
-	}
-	backend := make([]float32, t.vocabSize)
-	backend[idx] = 1
-	inputTensor := tensor.New(tensor.WithShape(t.vocabSize), tensor.WithBacking(backend))
-	node := G.NewVector(g, tensor.Float32, G.WithName(fmt.Sprintf("input_%v", t.offset)), G.WithShape(t.vocabSize), G.WithValue(inputTensor))
+	backend := make([]float32, s.vocabSize)
+	backend[s.sentence[s.offset]] = 1
+	inputTensor := tensor.New(tensor.WithShape(s.vocabSize), tensor.WithBacking(backend))
+	node := G.NewVector(g, tensor.Float32, G.WithName(fmt.Sprintf("input_%v", s.offset)), G.WithShape(s.vocabSize), G.WithValue(inputTensor))
+	s.offset++
 	return node, nil
 }
 
 // WriteComputedVector add the computed vectors to the output
-func (t *TrainingSet) WriteComputedVector(n *G.Node) error {
-	t.output = append(t.output, n)
+func (s *Section) WriteComputedVector(n *G.Node) error {
+	s.output = append(s.output, n)
 	return nil
 }
 
 // GetComputedVectors ..
-func (t *TrainingSet) GetComputedVectors() G.Nodes {
-	return t.output
+func (s *Section) GetComputedVectors() G.Nodes {
+	return s.output
 }
 
 // GetExpectedValue returns the encoded value of the rune next to the one present at offset
-func (t *TrainingSet) GetExpectedValue(offset int) (int, error) {
-	return 0, nil
+func (s *Section) GetExpectedValue(offset int) (int, error) {
+	return s.sentence[offset], nil
 }
 
 // GetTrainingSet is returning the self object with the
 // currentSection field adapted to it contains a full set of runes
-func (t *TrainingSet) GetTrainingSet() (*TrainingSet, error) {
-
-	return t, nil
+func (t *TrainingSet) GetTrainingSet() (*Section, error) {
+	section := &Section{
+		vocabSize: t.vocabSize,
+		offset:    0,
+		sentence:  make([]int, t.step),
+	}
+	buf := bufio.NewReader(t.rs)
+	for i := 0; i < t.step; i++ {
+		rne, _, err := buf.ReadRune()
+		idx, err := t.runeToIdx(rne)
+		if err != nil {
+			return nil, err
+		}
+		section.sentence[i] = idx
+	}
+	// TODO: set the offset to currentoffset+step
+	return section, nil
 }
