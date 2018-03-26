b0e3ffee6224fd5170c00b135460bd9a1347e8c7
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-03-26 15:24:00 +0200

    Warning

--- doc.go
+++ doc.go
@@ -1,5 +1,7 @@
 // Package lstm is a basic implementation of this kind of recural network.
+//
 // WARNING: This is only a skeleton of a new API for a LSTM implementation with Gorgonia.
-// Nothing is implemented at the present time. For a more advanced version, check
-// github.com/owulveryck/lstm
+// Nothing is implemented at the present time and the API may change.
+// There should be a working version with a less complete API working on the master
+// branch of the project
 package lstm
