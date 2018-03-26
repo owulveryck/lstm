f691399656957d4632a5abcc9cf4bafcad63eb5c
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-03-26 15:22:53 +0200

    Godoc

--- doc.go
+++ doc.go
@@ -1,7 +1,5 @@
-/*
-WARNING: This is only a skeleton of a new API for a LSTM implementation with Gorgonia.
-Nothing is implemented at the present time. For a more advanced version, check
-github.com/owulveryck/lstm
-*/
-
+// Package lstm is a basic implementation of this kind of recural network.
+// WARNING: This is only a skeleton of a new API for a LSTM implementation with Gorgonia.
+// Nothing is implemented at the present time. For a more advanced version, check
+// github.com/owulveryck/lstm
 package lstm
