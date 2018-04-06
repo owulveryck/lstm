2b7eae385d74d789018d259f4b779f8aad109638
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2018-04-06 09:10:34 +0200

    Doing a backup every now and then

--- example/train/train.go
+++ example/train/train.go
@@ -3,15 +3,21 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/gob"
 	"fmt"
 	"io"
 	"log"
 	"os"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/owulveryck/lstm"
 	"github.com/owulveryck/lstm/datasetter/char"
 	G "gorgonia.org/gorgonia"
 )
+
+type configuration struct {
+	Dump string `envconfig:"dump" default:"/tmp/dump.bin"`
+}
 
 func newVocabulary(filename string) (vocabulary, error) {
 	vocab := make(map[rune]struct{}, 0)
@@ -43,6 +49,11 @@ func newVocabulary(filename string) (vocabulary, error) {
 
 type vocabulary []rune
 
+type backup struct {
+	Model      lstm.Model
+	Vocabulary vocabulary
+}
+
 func (v vocabulary) runeToIdx(r rune) (int, error) {
 	for i := range v {
 		if v[i] == r {
@@ -61,6 +72,11 @@ func (v vocabulary) idxToRune(i int) (rune, error) {
 }
 
 func main() {
+	var config configuration
+	err := envconfig.Process("TRAIN", &config)
+	if err != nil {
+		log.Fatal(err)
+	}
 	filename := os.Args[1]
 	vocab, err := newVocabulary(filename)
 	if err != nil {
@@ -80,19 +96,44 @@ func main() {
 		}
 		max, _ := f.Seek(0, io.SeekEnd)
 		f.Seek(0, io.SeekStart)
-		tset := char.NewTrainingSet(f, vocab.runeToIdx, vocabSize, 25, 1)
+		tset := char.NewTrainingSet(f, vocab.runeToIdx, vocabSize, 30, 1)
 		pause := make(chan struct{})
 		infoChan, errc := model.Train(context.TODO(), tset, solver, pause)
 		iter := 1
+		var minLoss float32
 		for infos := range infoChan {
 			if iter%100 == 0 {
+				if minLoss == 0 {
+					minLoss = infos.Cost
+				}
+				if infos.Cost < minLoss {
+					minLoss = infos.Cost
+					log.Println("Backup because loss is minimum")
+					bkp := backup{
+						Model:      *model,
+						Vocabulary: vocab,
+					}
+					f, err := os.OpenFile(config.Dump, os.O_RDWR|os.O_CREATE, 0755)
+					if err != nil {
+						log.Println(err)
+					}
+					enc := gob.NewEncoder(f)
+					err = enc.Encode(bkp)
+					if err != nil {
+						log.Println(err)
+					}
+					if err := f.Close(); err != nil {
+						log.Println(err)
+					}
+					// Do a backup
+				}
 				here, _ := f.Seek(0, io.SeekCurrent)
 				fmt.Printf("[%v/%v]%v\n", here, max, infos)
 			}
 			if iter%500 == 0 {
 				fmt.Println("\nGoing to predict")
 				pause <- struct{}{}
-				prediction := char.NewPrediction("B", vocab.runeToIdx, 500, vocabSize)
+				prediction := char.NewPrediction("B", vocab.runeToIdx, 100, vocabSize)
 				err := model.Predict(context.TODO(), prediction)
 				if err != nil {
 					log.Println(err)
