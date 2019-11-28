e1adf7ca1e049bb44e95994f2de15b4eb9f40eac
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-11-28 23:00:21 +0100

    feat: flags

--- cmd/train/main.go
+++ cmd/train/main.go
@@ -5,17 +5,38 @@ import (
 	"log"
 	"os"
 
+	"github.com/kelseyhightower/envconfig"
 	"github.com/owulveryck/lstm"
 )
 
+type configuration struct {
+	HiddenSize int `envconfig:"HIDDEN_SIZE" default:"1024" required:"true"`
+	Epoch      int `envconfig:"EPOCH" default:"100" required:"true"`
+	BatchSize  int `envconfig:"BATCH_SIZE" default:"100" required:"true"`
+}
+
+func usage() error {
+	flag.Usage()
+	var config configuration
+	return envconfig.Usage("lstm", &config)
+}
+
 func main() {
 	var inputFile, outputFile string
+	var help bool
+	var config configuration
 	flag.StringVar(&inputFile, "i", "", "input file")
 	flag.StringVar(&outputFile, "o", "backup.bin", "output file")
+	flag.BoolVar(&help, "h", false, "help")
 	flag.Parse()
-	if inputFile == "" {
-		flag.Usage()
-		os.Exit(1)
+	if help || inputFile == "" {
+		usage()
+		return
+	}
+
+	err := envconfig.Process("lstm", &config)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
 
 	f, err := os.Open(inputFile)
@@ -25,9 +46,8 @@ func main() {
 	dict := getVocabulary(f)
 	defer f.Close()
 	vectorSize := len(dict)
-	hiddenSize := 1024
 
-	nn := lstm.NewLSTM(vectorSize, hiddenSize)
+	nn := lstm.NewLSTM(vectorSize, config.HiddenSize)
 	nn.Dict = dict
 	initLearnables(nn.Learnables())
 }
