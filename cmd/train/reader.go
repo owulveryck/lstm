48f2adb7fbb74de2b745ea424849e1384f24a21f
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-11-29 16:13:06 +0100

    feat: taking an io.ReadSeeker as argument for move

--- cmd/train/reader.go
+++ cmd/train/reader.go
@@ -1,20 +1,19 @@
 package main
 
-import "io"
-
-type reader interface {
-	io.ReadSeeker
-	io.RuneReader
-}
+import (
+	"bufio"
+	"io"
+)
 
 // move the seeker back from seekBack (in number of bytes) and goes forward of step *runes*
-func move(rs reader, step int, seekBack int64) error {
+func move(rs io.ReadSeeker, step int, seekBack int64) error {
+	buf := bufio.NewReader(rs)
 	_, err := rs.Seek(-seekBack, io.SeekCurrent)
 	if err != nil {
 		return err
 	}
 	for i := 0; i < step; i++ {
-		_, _, err := rs.ReadRune()
+		_, _, err := buf.ReadRune()
 		if err != nil {
 			return err
 		}
