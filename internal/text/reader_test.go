7448fba18d8aa75f9122ab7f944d5d012912e522
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-11-30 09:19:33 +0100

    wip: move function is not working as expected

--- internal/text/reader_test.go
+++ internal/text/reader_test.go
@@ -3,13 +3,13 @@ package text
 import (
 	"bytes"
 	"io"
-	"log"
 	"testing"
 )
 
 func TestMove(t *testing.T) {
 	testinput := bytes.NewReader([]byte(`abc⌘abc⌘abc⌘abc⌘abc⌘`))
 	step := 4
+	i := 0
 	for {
 		rn, n, err := testinput.ReadRune()
 		if err != nil {
@@ -21,9 +21,16 @@ func TestMove(t *testing.T) {
 		if rn != 'a' {
 			t.Fatal(rn)
 		}
+		i++
 		err = move(testinput, step, int64(n))
 		if err != nil {
-			log.Fatal(err)
+			if err != io.EOF {
+				t.Fatal(err)
+			}
+			break
 		}
+	}
+	if i != 5 {
+		t.Fatalf("expected 5 elements, but got %v", i)
 	}
 }
