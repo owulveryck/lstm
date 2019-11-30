ba9c94bdf938ad76fbd53d980d5c89dcdf2aeba4
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-11-30 14:13:46 +0100

    feat: code refactoring

--- cmd/train/save_test.go
+++ cmd/train/save_test.go
@@ -1,4 +1,4 @@
-package text
+package main
 
 import (
 	"io/ioutil"
--- cmd/train/train.go
+++ cmd/train/train.go
@@ -1,37 +1,13 @@
 package main
 
 import (
+	"context"
 	"io"
 
 	"github.com/owulveryck/lstm"
-	"gorgonia.org/tensor"
 )
 
-func train(nn *lstm.LSTM, input io.ReadSeeker, config configuration) error {
+func train(ctx context.Context, nn *lstm.LSTM, input io.ReadSeeker, config configuration) error {
 
-	ds := newDataset(input, nn.Dict)
-
-	x := tensor.NewDense(tensor.Float64, []int{len(nn.Dict), config.BatchSize})
-	for epoch := 0; epoch < config.Epoch; epoch++ {
-		if _, err := input.Seek(0, io.SeekStart); err != nil {
-			return err
-		}
-		for {
-			n, err := ds.read(x)
-			if err != nil {
-				if err == io.EOF {
-					continue
-				}
-				return err
-			}
-			err = move(input, config.Step, int64(n))
-			if err != nil {
-				if err == io.EOF {
-					continue
-				}
-				return err
-			}
-		}
-	}
 	return nil
 }
