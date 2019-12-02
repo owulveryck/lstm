3017d49d57d6b3a01e1ee36d39202fd922278746
Olivier Wulveryck <olivier.wulveryck@gmail.com>
2019-12-02 21:54:52 +0100

    feat: add cross entropy loss

--- network.go
+++ network.go
@@ -1,6 +1,7 @@
 package lstm
 
 import (
+	"errors"
 	"fmt"
 
 	"gorgonia.org/gorgonia"
@@ -13,7 +14,6 @@ type Network struct {
 	H    []*gorgonia.Node
 	C    []*gorgonia.Node
 	Y    []*gorgonia.Node
-	cost *gorgonia.Node
 }
 
 func NewNetwork(nn *LSTM, layers int) *Network {
@@ -31,7 +31,7 @@ func NewNetwork(nn *LSTM, layers int) *Network {
 		x[i] = gorgonia.NewVector(nn.G, gorgonia.Float64, gorgonia.WithName(fmt.Sprintf("xₜ+%v", i)),
 			gorgonia.WithShape(vectorSize))
 		h[i+1], c[i+1] = nn.NewCell(x[i], h[i], c[i])
-		y[i] = nn.LogProb(h[i+1])
+		y[i] = nn.Dense(h[i+0])
 	}
 	return &Network{
 		lstm: nn,
@@ -40,4 +40,23 @@ func NewNetwork(nn *LSTM, layers int) *Network {
 		C:    c,
 		Y:    y,
 	}
+}
+
+func (n *Network) Cost(target []*gorgonia.Node) (cost *gorgonia.Node, err error) {
+	if len(target) != len(n.Y) {
+		return nil, errors.New("target and Y are of different size")
+	}
+	var loss *gorgonia.Node
+	for i := 0; i < len(n.Y); i++ {
+		loss = neg(mul(target[i], logarithm(n.Y[i])))
+
+		if cost == nil {
+			cost = loss
+		} else {
+			cost = add(cost, loss)
+		}
+		gorgonia.WithName("Cost")(cost)
+
+	}
+	return cost, nil
 }
--- network_test.go
+++ network_test.go
@@ -2,10 +2,13 @@ package lstm
 
 import (
 	"io/ioutil"
+	"math"
 	"os"
 	"testing"
 
+	"gorgonia.org/gorgonia"
 	"gorgonia.org/gorgonia/encoding/dot"
+	"gorgonia.org/tensor"
 )
 
 func TestNewNetwork(t *testing.T) {
@@ -44,4 +47,68 @@ func TestNetwork_dot(t *testing.T) {
 	}
 
 	t.Log("content dumped into ", dotFile.Name())
+}
+
+func TestCost(t *testing.T) {
+	dim := 3
+	g := gorgonia.NewGraph()
+	n := &Network{
+		Y: []*gorgonia.Node{
+			gorgonia.NewVector(g, float, gorgonia.WithName("yy0"),
+				gorgonia.WithShape(dim)),
+			gorgonia.NewVector(g, float, gorgonia.WithName("yy1"),
+				gorgonia.WithShape(dim)),
+		},
+	}
+	y := []*gorgonia.Node{
+		gorgonia.NewVector(g, float, gorgonia.WithName("y0"),
+			gorgonia.WithShape(dim)),
+		gorgonia.NewVector(g, float, gorgonia.WithName("y1"),
+			gorgonia.WithShape(dim)),
+	}
+	yT1 := tensor.NewDense(float, []int{dim}, tensor.WithBacking([]float64{1, 0, 0}))
+	yT2 := tensor.NewDense(float, []int{dim}, tensor.WithBacking([]float64{0, 1, 0}))
+	yyT1 := tensor.NewDense(float, []int{dim}, tensor.WithBacking([]float64{0.5, 0.1, 0.4}))
+	yyT2 := tensor.NewDense(float, []int{dim}, tensor.WithBacking([]float64{0.2, 0.6, 0.2}))
+	gorgonia.Let(n.Y[0], yyT1)
+	gorgonia.Let(n.Y[1], yyT2)
+	gorgonia.Let(y[0], yT1)
+	gorgonia.Let(y[1], yT2)
+	cost, err := n.Cost(y)
+	if err != nil {
+		t.Fatal(err)
+	}
+	vm := gorgonia.NewTapeMachine(g)
+	err = vm.RunAll()
+	if err != nil {
+		t.Fatal(err)
+	}
+	v, ok := cost.Value().Data().(float64)
+	if !ok {
+		t.Fatal("Expected a scalar value for cost")
+	}
+	if v != -(math.Log(0.5) + math.Log(0.6)) {
+		t.Fatal(v)
+	}
+
+}
+func TestCost_err(t *testing.T) {
+	dim := 3
+	g := gorgonia.NewGraph()
+	n := &Network{
+		Y: []*gorgonia.Node{
+			gorgonia.NewVector(g, float, gorgonia.WithName("yy0"),
+				gorgonia.WithShape(dim)),
+			gorgonia.NewVector(g, float, gorgonia.WithName("yy1"),
+				gorgonia.WithShape(dim)),
+		},
+	}
+	y := []*gorgonia.Node{
+		gorgonia.NewVector(g, float, gorgonia.WithName("y1"),
+			gorgonia.WithShape(dim)),
+	}
+	_, err := n.Cost(y)
+	if err == nil {
+		t.Fail()
+	}
 }
